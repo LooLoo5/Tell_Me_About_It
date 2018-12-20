c97cd27ebb12bf9eafc661ac30a9441b76f23194
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-12-19 21:20:11 -0600

    added js files and routes

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const request = require('request');
 const cheerio = require('cheerio');
-
 const axios = require("axios");
-const mongojs = require("mongojs");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 const url = "https://www.nytimes.com/";
 
-// Database configuration
-const databaseUrl = "scraper";
-const collections = ["scrapedData"];
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-axios.get("https://old.reddit.com/r/webdev").then(function(response) {
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+axios.get("https://www.nytimes.com/").then(function(response) {
 
   const $ = cheerio.load(response.data);
 
