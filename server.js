2ed17fee19fce0c384f7e99b8c45887d497b0d58
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-12-19 18:52:08 -0600

    added index and two packages

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+const axios = require("axios");
+const mongojs = require("mongojs");
+
+const app = express();
+const url = "https://www.nytimes.com/";
+
+// Database configuration
+const databaseUrl = "scraper";
+const collections = ["scrapedData"];
+
+axios.get("https://old.reddit.com/r/webdev").then(function(response) {
+
+  const $ = cheerio.load(response.data);
+
+  const results = [];
+
+  $("p.title").each(function(i, element) {
+
+    const title = $(element).text();
+
+    const link = $(element).children().attr("href");
+
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+
+  console.log(results);
+});
